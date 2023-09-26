9f2fbc3de1631f60dead02d861180788add543d1
codepraycode <preciousolusola16@gmail.com>
2023-09-26 01:43:37 +0100

    Updated development mode

--- index.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-import Console from "./src/console";
-
-
-// Display console information first
-Console.help([]);
-
-
-Console.run();
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+// import Console from "./console";
+
+
+// Display console information first
+// Console.help([]);
+// Console.run();
+
+
+console.log("Fresh console")
